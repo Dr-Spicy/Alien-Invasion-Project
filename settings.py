ddd8f901a102789714cbbe9e4fbc0a41a0720d46
Xiaohan Liu <xhliu127@yahoo.com>
2023-07-25 19:16:15 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,18 +9,18 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 2.5
-        self.ship_limit = 1
+        self.ship_speed = 4.
+        self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 4.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.buttets_allowed = 6
+        self.buttets_allowed = 5
 
         # Alien settings
-        self.alien_speed = 1.
+        self.alien_speed = 2.
         # fleet_drop_spd controls how quickly it drops when hitting the right
         # edge
         self.fleet_drop_speed = 10
